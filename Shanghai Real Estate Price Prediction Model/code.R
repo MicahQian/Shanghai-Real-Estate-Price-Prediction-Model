872c9e74e8c3be7d42965f87eb41fbba98bcadd8
MicahQian <32171819+MicahQian@users.noreply.github.com>
2018-02-22 12:44:30 +0000

    second draft

--- Shanghai Real Estate Price Prediction Model/code.R
+++ Shanghai Real Estate Price Prediction Model/code.R
@@ -2,3 +2,15 @@ X=read.csv("F:\\data.csv")
 attach(X) 
 plot(t,y)
 
+y1=log(y)
+a1=log(t)
+reg=lm(y1~t)
+summary(reg)
+a1= -3.281e+02
+a=exp(a1)
+b= 1.681e-01 
+yy=a*exp(b*t)
+plot(t,y)
+lines(t,yy)
+plot(X)
+
